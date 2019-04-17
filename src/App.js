190dc419ba015e6556f993661484c3dd2827f9a8
Aleksandr <duxard@gmail.com>
2019-04-17 01:03:22 -0400

    redux init

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
-
+import CreateProject from './components/projects/CreateProject';
+import './index.css';
 class App extends Component {
+
   render() {
     return (
       <BrowserRouter>
@@ -17,6 +19,7 @@ class App extends Component {
             <Route path='/project/:id' component={ProjectDetails} />
             <Route path='/signin' component={SignIn} />
             <Route path='/signup' component={SignUp} />
+            <Route path='/create' component={CreateProject} />
           </Switch>
         </div>
       </BrowserRouter>
