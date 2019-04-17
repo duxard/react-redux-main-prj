8ec319013213c5d55ed070a00e43467fdaa398e4
Aleksandr <duxard@gmail.com>
2019-04-17 01:20:53 -0400

    step #12

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+  projects: [
+    {id: 1, title: 'title one', content: 'lorem ipsom dolores'},
+    {id: 2, title: 'title two', content: 'lorem ipsom dolores'},
+    {id: 3, title: 'title three', content: 'lorem ipsom dolores'}
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
