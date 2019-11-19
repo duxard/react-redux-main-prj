2ce370d052dba13de2fa22dd2252f550dbe3dcdb
Aleksandr <duxard@gmail.com>
2019-11-19 00:30:48 -0500

    step 18 - adding data to mongo db

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,27 @@
+import axios from 'axios';
+
+import {
+  CREATE_PROJECT,
+  CREATE_PROJECT_ERROR
+} from './types';
+
+const url = 'https://asta-web-1.herokuapp.com/api';
+
 export const createProject = (project) => {
-  return (dispatch, getState) => {
-    dispatch({type: 'CREATE_PROJECT', project});
+  return (dispatch) => {
+    axios
+      .post(`${url}/projects`, {
+        ...project,
+        authorFirstName: 'Alex',
+        authorLastName: 'Starchenko',
+        authorId: 12345,
+        createdAt: new Date()
+      })
+      .then(() => {
+        dispatch({type: CREATE_PROJECT, project});
+      })
+      .catch((err) => {
+        dispatch({type: CREATE_PROJECT_ERROR, err});
+      });
   }
 };
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,3 +1,8 @@
+import {
+  CREATE_PROJECT,
+  CREATE_PROJECT_ERROR
+} from '../actions/types';
+
 const initState = {
   projects: [
     {id: 1, title: 'title one', content: 'lorem ipsom dolores'},
@@ -8,10 +13,15 @@ const initState = {
 
 const projectReducer = (state = initState, action) => {
   switch( action.type ) {
-    case 'CREATE_PROJECT':
-      console.log('creacted project', action.project);
+    case CREATE_PROJECT:
+      console.log(`Created project: ${action.project}`);
+      return state;
+    case CREATE_PROJECT_ERROR:
+      console.log(`Create project error: ${action.err}`);
+      return state;
+    default:
+      return state;
   }
-  return state;
 }
 
 export default projectReducer;
